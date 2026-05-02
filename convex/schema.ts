a45bd39a6d4a03425c3ed264a2568ce1c3d873e0
unknown <myriahvielle619@gmail.com>
2026-05-02 10:22:56 +0800

    Done updating the users schema to include fullname

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,15 +2,14 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  todos: defineTable({
+    text: v.string(),
+    isCompleted: v.boolean(),
+    userId: v.id("users")
+  }),
   users: defineTable({
     fullname: v.string(),
     username: v.string(),
     password: v.string(),
-  }),
-
-  todos: defineTable({
-    text: v.string(),
-    isCompleted: v.boolean(),
-    userId: v.id("users"),
-  }),
+  })
 });
